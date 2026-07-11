12b4e4dd6ce3b81b47dbb96a1694d8630beef71b
Kursad Cula <kursadben@gmail.com>
2026-07-12 01:32:46 +0300

    Fix Betnex API endpoints

---
+++ api/providers.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://livecasinoapi.betnex.co/casino/getallproviders",
+      {
+        headers: {
+          "x-betnex-key": process.env.BETNEX_API_KEY
+        }
+      }
+    );
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
